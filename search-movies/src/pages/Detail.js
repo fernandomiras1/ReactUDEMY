c37cfdf0591f09facf8a7be3d5465a80cc77a6aa
Fernando Miras <@fernando.miras>
2019-12-02 17:34:09 -0300

    todo

--- search-movies/src/pages/Detail.js
+++ search-movies/src/pages/Detail.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+const API_KEY = '4287ad07'
 export class Detail extends Component {
     static propTypes = {
         id: PropTypes.string
     }
 
-    _getMovieById(id) {
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMove}`)
+    state = { movie: {} }
+
+    _getMovieById({id}) {
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`)
         .then(res => res.json())
-        .then(resu => {
-            const { Search = [], totalResults = '0'} = resu
-            console.log({ Search, totalResults });
-            // Se lo pasamos a la props para que lo trabaje el Padre App.js
-            this.props.onResults(Search)
+        .then(movie => {
+            console.log({movie});
+            this.setState({ movie })
         })
     }
 
@@ -23,8 +24,16 @@ export class Detail extends Component {
         this._getMovieById({ id })
     }
     render() {
+        const { Title, Poster, Poster, Metascore, Plot } =
+            this.state.movie
+
         return (
-            <p>pagina Detalle</p>
+            <div>
+                <h1>{Title}</h1>
+                <img src={Poster}></img>
+                <h3>{Actors}</h3>
+                <p>pagina Detalle</p>
+            </div>
         )
     }    
 }
