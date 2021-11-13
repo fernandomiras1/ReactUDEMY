9e7d85dcd45993fd65ae6d852cc47c4d9ff41ae8
fernando.miras <fernando.miras.pc@gmail.com>
2021-11-13 10:30:51 -0300

    feat: add enzyme

--- react-random/app-calculator/src/components/NumbeRotation/NumbeRotation.js
+++ react-random/app-calculator/src/components/NumbeRotation/NumbeRotation.js
@@ -1,25 +1,24 @@
 import React,  { useState } from 'react';
-import PropTypes from 'prop-types';
 import './NumbeRotation.css';
 
 const NumbeRotation = () => {
     const [numberRotation, setNumberRotation ] = useState([1, 2, 3, 4, 5])
-    
+    const [value, setValue] = useState('')
+
     const numbersRotation = (numbers) => {
         numbers.push(numbers.shift())
         return numbers
     }
 
     const handleRotation = () => {
-        console.log(numbersRotation(numberRotation));
         setNumberRotation(numbersRotation(numberRotation))
+        setValue(numberRotation.toString())
     }
 
-    
     return (
         <div className="number-rotation-container">
         <strong>Numbers Rotation:</strong>
-        <p className="number-rotation-value">{numberRotation.toString()}</p>
+        <p className="number-rotation-value">{value}</p>
         <button type="button" onClick={handleRotation}>Rotation</button>
       </div>
     )
