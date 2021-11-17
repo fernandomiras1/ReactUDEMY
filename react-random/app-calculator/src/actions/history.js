f0ee057997e0749ddfbef77c55ce16875be61b9a
fernando.miras <fernando.miras.pc@gmail.com>
2021-11-17 20:18:34 -0300

    delete random-string library

--- react-random/app-calculator/src/actions/history.js
+++ react-random/app-calculator/src/actions/history.js
@@ -1,10 +1,27 @@
 export const ADD_HISTORY_ITEM = 'ADD_HISTORY_ITEM';
 
+const parseExpr = (expr) => {
+  
+  const operator = ['/', '*', '-', '+'].find(x => expr.includes(x))
+  if (operator) {
+    const position = expr.indexOf(operator) + 1;
+
+    if (position === expr.length) {
+      const number = expr.slice(0 , expr.indexOf(operator));
+      const newExpr = number + operator + number
+      // eslint-disable-next-line no-eval
+      return { result: eval(newExpr), expr: newExpr }
+    }
+  }
+
+  // eslint-disable-next-line no-eval
+  return { result: eval(expr), expr }
+}
+
 export const addHistoryItem = (expr) => ({
   type: ADD_HISTORY_ITEM,
   payload: {
-    expr,
-    // eslint-disable-next-line no-eval
-    result: eval(expr)
+    expr: parseExpr(expr).expr,
+    result: parseExpr(expr).result
   }
 })
