2d7d69a2d95515d491b68b1403cba192b3ffed64
Fernando Miras <f.miras@electroneek.com>
2023-02-24 00:06:42 -0300

    BIL-2198 React Random ts

--- react-random/galicia-app-ts/pages/download-qr/index.tsx
+++ react-random/galicia-app-ts/pages/download-qr/index.tsx
@@ -1,41 +1,98 @@
-import React, { Fragment, useEffect, useRef } from "react";
-import { Loading, Card, Grid, Text, Link, Button } from "@nextui-org/react";
-
-import { Layout } from "../../components/layouts";
-import { useDelay } from "../../hooks/useDelay";
-import { UICard } from "../../components/ui";
+import React, { Fragment, useEffect, useRef, useState } from "react";
 import {
+  Loading,
+  Card,
+  Grid,
+  Text,
+  Link,
+  Button,
+  Image,
+  Modal,
+  useModal,
+  Container,
+} from "@nextui-org/react";
+import { Layout } from "../../components/layouts";
+
+import {
+  generateElementSimpleQRCode,
+  generateElementDobleQRCode,
   downloadMultipleQR,
   downloadQRCode,
 } from "../../utils/download-qr-code";
+import { useForm } from "../../hooks/useForm";
+
+const dataDownloadQR = [
+  {
+    qrName: "Sucursal San Justo - QR SJ 1",
+    qr: "iVBORw0KGgoAAAANSUhEUgAAAsUAAALFAQAAAAADsKO1AAAEF0lEQVR42u3dy44bIRCFYd6A939LltmRkdoN5xS47UiJFFG/FUWenvbnWaGmbpT+j16/CjIyMjIyMjIyMjIyMjIyMjIyMjLyfy23Ml7Xj68rtV///7xev7pu/rleX2/mzePjyMjIOeT55rrz0oYsn53s0NriICMjZ5Cb3KP4ffH68bpyrTx2Q/OvQEZGTiW3uS3SB5Xxq+0jDTIycmp5LDt1fjbsj2wH1JGRkbPKugO6r1w3v9acZQmaH/x+b4WMjHyQbGGQP//3VSwXGRn5JNlfIztjS5DEY0ceRyMq3+SOkZGRz5HbXI5e0dSx2Qkv3/jYLqnKtyAjIx8vy/5l3jbWpTvVO2KwuhzNiCsyMnIyeWixCOReoAy/FytN/u4jrsjIyOfKWnoaikPmZ6utSPEvqR0ZGTmRrJsaKT21avZQSKZpX42rICMjp5F1FzMSLjEeIjlf2yK1JfqKjIycQ57hU4mZzHCKt72sKZunzhdkZOQTZVt2JEgytW6p3nlDtcpVZGTkRLJXp9uGqNhzjjXTaQPdmwp2ZGTkM+Xu6drewyOK7YO2Gd7+VBmCjIx8pKydcfqgMnO4d0o3boI0wPKYO0ZGRj5MXks+1qIyS9zIxudD7hgZGflIWRrztfNFm2pD160OGYv9tsjIyGnkoMWull0F+7xSPWyLjIx8vNytpjTMBdLiMQ3GhnqzfYYXGRn5YNlr1+0JR/M4a0lq2Y0GQkZGziCX3XjSVtYkb4jBfph4jIyMfKosaZdZBKLbnLJMBJKyVS0pQUZGTiR364AbXbRhxqClbPruKChkZOQ08pq0tTY6X5HseLi2G0WIjIx8vBx8/Xjz09+qdehvRiUjIyPnkduy39GGuOoHsoQK9lCThoyMnEZ+23TfbBJyOK5aH2berUjIyMgHyhpidWTT+aJDgdaQLDIychq5h7E/xWOwocCsLgWr5ekESWRk5ANl3fVIE1xM4tQeMrw9HL7QOzIychbZT3zToxNiC612u/jX6chTZGTk8+XubGjYD0exNHu20QwvMjJyInntwV8a4uLwUm2y8wFByMjISWSNinQ9R9IzvDHMonul/v5MW2Rk5PNkPwVSx6RbnlfLRbyy/SniioyMfKrszyr2xFJ7qFe3m8MfhoyMnET2ExZC0WnYB9l65SsSMjJyInnpf4lT0H3sz9z7fOzQR0ZGPlX2xrdNE9xa3N58EFB77NBHRkY+UbY3rWhu96FDX2vXn09YQEZGPlBucQJh7Nb3ice6IYrhVmRk5JTympfRdrnuxzHEIerIyMgp5dh1q4HW3ZFwlqZBRkZOIneLh9hx9j47fbP+tBKKVJGRkTPIcdxxteuaoLFOmeVmZGTkLPJffyEjIyMjIyMjIyMjIyMjIyMjIyMjp5N/A7C8eKsv3GtDAAAAAElFTkSuQmCC",
+  },
+  {
+    qrName: "Sucursal San Justo - QR SJ 2",
+    qr: "iVBORw0KGgoAAAANSUhEUgAAAsUAAALFAQAAAAADsKO1AAAEF0lEQVR42u3dy44bIRCFYd6A939LltmRkdoN5xS47UiJFFG/FUWenvbnWaGmbpT+j16/CjIyMjIyMjIyMjIyMjIyMjIyMjLyfy23Ml7Xj68rtV///7xev7pu/rleX2/mzePjyMjIOeT55rrz0oYsn53s0NriICMjZ5Cb3KP4ffH68bpyrTx2Q/OvQEZGTiW3uS3SB5Xxq+0jDTIycmp5LDt1fjbsj2wH1JGRkbPKugO6r1w3v9acZQmaH/x+b4WMjHyQbGGQP//3VSwXGRn5JNlfIztjS5DEY0ceRyMq3+SOkZGRz5HbXI5e0dSx2Qkv3/jYLqnKtyAjIx8vy/5l3jbWpTvVO2KwuhzNiCsyMnIyeWixCOReoAy/FytN/u4jrsjIyOfKWnoaikPmZ6utSPEvqR0ZGTmRrJsaKT21avZQSKZpX42rICMjp5F1FzMSLjEeIjlf2yK1JfqKjIycQ57hU4mZzHCKt72sKZunzhdkZOQTZVt2JEgytW6p3nlDtcpVZGTkRLJXp9uGqNhzjjXTaQPdmwp2ZGTkM+Xu6drewyOK7YO2Gd7+VBmCjIx8pKydcfqgMnO4d0o3boI0wPKYO0ZGRj5MXks+1qIyS9zIxudD7hgZGflIWRrztfNFm2pD160OGYv9tsjIyGnkoMWull0F+7xSPWyLjIx8vNytpjTMBdLiMQ3GhnqzfYYXGRn5YNlr1+0JR/M4a0lq2Y0GQkZGziCX3XjSVtYkb4jBfph4jIyMfKosaZdZBKLbnLJMBJKyVS0pQUZGTiR364AbXbRhxqClbPruKChkZOQ08pq0tTY6X5HseLi2G0WIjIx8vBx8/Xjz09+qdehvRiUjIyPnkduy39GGuOoHsoQK9lCThoyMnEZ+23TfbBJyOK5aH2berUjIyMgHyhpidWTT+aJDgdaQLDIychq5h7E/xWOwocCsLgWr5ekESWRk5ANl3fVIE1xM4tQeMrw9HL7QOzIychbZT3zToxNiC612u/jX6chTZGTk8+XubGjYD0exNHu20QwvMjJyInntwV8a4uLwUm2y8wFByMjISWSNinQ9R9IzvDHMonul/v5MW2Rk5PNkPwVSx6RbnlfLRbyy/SniioyMfKrszyr2xFJ7qFe3m8MfhoyMnET2ExZC0WnYB9l65SsSMjJyInnpf4lT0H3sz9z7fOzQR0ZGPlX2xrdNE9xa3N58EFB77NBHRkY+UbY3rWhu96FDX2vXn09YQEZGPlBucQJh7Nb3ice6IYrhVmRk5JTympfRdrnuxzHEIerIyMgp5dh1q4HW3ZFwlqZBRkZOIneLh9hx9j47fbP+tBKKVJGRkTPIcdxxteuaoLFOmeVmZGTkLPJffyEjIyMjIyMjIyMjIyMjIyMjIyMjp5N/A7C8eKsv3GtDAAAAAElFTkSuQmCC",
+  },
+];
+export type formData = {
+  xContentQR: number;
+  yContentQR: number;
+  xNameQR: number;
+  yNameQR: number;
+};
+
+const formData: formData = {
+  xContentQR: 500,
+  yContentQR: 950,
+  xNameQR: 430,
+  yNameQR: 1620,
+};
 
 const DownloadQrPage = () => {
+  const { setVisible, bindings } = useModal();
+  const [base64, setbase64] = useState("");
+  const { formState, onInputChange } = useForm(formData);
+
+  const { xContentQR, yContentQR, xNameQR, yNameQR } = formState;
   const onDownloadSimpleQR = () => {
     console.log("onDownloadSimpleQR");
-
-    const dataDownloadQR = [
-      {
-        name: "Sucursal San Justo - QR SJ 1",
-        content:
-          "iVBORw0KGgoAAAANSUhEUgAAAsUAAALFAQAAAAADsKO1AAAEF0lEQVR42u3dy44bIRCFYd6A939LltmRkdoN5xS47UiJFFG/FUWenvbnWaGmbpT+j16/CjIyMjIyMjIyMjIyMjIyMjIyMjLyfy23Ml7Xj68rtV///7xev7pu/rleX2/mzePjyMjIOeT55rrz0oYsn53s0NriICMjZ5Cb3KP4ffH68bpyrTx2Q/OvQEZGTiW3uS3SB5Xxq+0jDTIycmp5LDt1fjbsj2wH1JGRkbPKugO6r1w3v9acZQmaH/x+b4WMjHyQbGGQP//3VSwXGRn5JNlfIztjS5DEY0ceRyMq3+SOkZGRz5HbXI5e0dSx2Qkv3/jYLqnKtyAjIx8vy/5l3jbWpTvVO2KwuhzNiCsyMnIyeWixCOReoAy/FytN/u4jrsjIyOfKWnoaikPmZ6utSPEvqR0ZGTmRrJsaKT21avZQSKZpX42rICMjp5F1FzMSLjEeIjlf2yK1JfqKjIycQ57hU4mZzHCKt72sKZunzhdkZOQTZVt2JEgytW6p3nlDtcpVZGTkRLJXp9uGqNhzjjXTaQPdmwp2ZGTkM+Xu6drewyOK7YO2Gd7+VBmCjIx8pKydcfqgMnO4d0o3boI0wPKYO0ZGRj5MXks+1qIyS9zIxudD7hgZGflIWRrztfNFm2pD160OGYv9tsjIyGnkoMWull0F+7xSPWyLjIx8vNytpjTMBdLiMQ3GhnqzfYYXGRn5YNlr1+0JR/M4a0lq2Y0GQkZGziCX3XjSVtYkb4jBfph4jIyMfKosaZdZBKLbnLJMBJKyVS0pQUZGTiR364AbXbRhxqClbPruKChkZOQ08pq0tTY6X5HseLi2G0WIjIx8vBx8/Xjz09+qdehvRiUjIyPnkduy39GGuOoHsoQK9lCThoyMnEZ+23TfbBJyOK5aH2berUjIyMgHyhpidWTT+aJDgdaQLDIychq5h7E/xWOwocCsLgWr5ekESWRk5ANl3fVIE1xM4tQeMrw9HL7QOzIychbZT3zToxNiC612u/jX6chTZGTk8+XubGjYD0exNHu20QwvMjJyInntwV8a4uLwUm2y8wFByMjISWSNinQ9R9IzvDHMonul/v5MW2Rk5PNkPwVSx6RbnlfLRbyy/SniioyMfKrszyr2xFJ7qFe3m8MfhoyMnET2ExZC0WnYB9l65SsSMjJyInnpf4lT0H3sz9z7fOzQR0ZGPlX2xrdNE9xa3N58EFB77NBHRkY+UbY3rWhu96FDX2vXn09YQEZGPlBucQJh7Nb3ice6IYrhVmRk5JTympfRdrnuxzHEIerIyMgp5dh1q4HW3ZFwlqZBRkZOIneLh9hx9j47fbP+tBKKVJGRkTPIcdxxteuaoLFOmeVmZGTkLPJffyEjIyMjIyMjIyMjIyMjIyMjIyMjp5N/A7C8eKsv3GtDAAAAAElFTkSuQmCC",
-      },
-    ];
-    downloadQRCode(dataDownloadQR[0].content, `${dataDownloadQR[0].name}`);
+    downloadQRCode(dataDownloadQR[0].qr, `${dataDownloadQR[0].qrName}`);
   };
 
   const onDownloadDobleQR = () => {
     console.log("Descagar Doble QR");
-    const dataDownloadQR = [
-      {
-        qrName: "Sucursal San Justo - QR SJ 1",
-        qr: "iVBORw0KGgoAAAANSUhEUgAAAsUAAALFAQAAAAADsKO1AAAEF0lEQVR42u3dy44bIRCFYd6A939LltmRkdoN5xS47UiJFFG/FUWenvbnWaGmbpT+j16/CjIyMjIyMjIyMjIyMjIyMjIyMjLyfy23Ml7Xj68rtV///7xev7pu/rleX2/mzePjyMjIOeT55rrz0oYsn53s0NriICMjZ5Cb3KP4ffH68bpyrTx2Q/OvQEZGTiW3uS3SB5Xxq+0jDTIycmp5LDt1fjbsj2wH1JGRkbPKugO6r1w3v9acZQmaH/x+b4WMjHyQbGGQP//3VSwXGRn5JNlfIztjS5DEY0ceRyMq3+SOkZGRz5HbXI5e0dSx2Qkv3/jYLqnKtyAjIx8vy/5l3jbWpTvVO2KwuhzNiCsyMnIyeWixCOReoAy/FytN/u4jrsjIyOfKWnoaikPmZ6utSPEvqR0ZGTmRrJsaKT21avZQSKZpX42rICMjp5F1FzMSLjEeIjlf2yK1JfqKjIycQ57hU4mZzHCKt72sKZunzhdkZOQTZVt2JEgytW6p3nlDtcpVZGTkRLJXp9uGqNhzjjXTaQPdmwp2ZGTkM+Xu6drewyOK7YO2Gd7+VBmCjIx8pKydcfqgMnO4d0o3boI0wPKYO0ZGRj5MXks+1qIyS9zIxudD7hgZGflIWRrztfNFm2pD160OGYv9tsjIyGnkoMWull0F+7xSPWyLjIx8vNytpjTMBdLiMQ3GhnqzfYYXGRn5YNlr1+0JR/M4a0lq2Y0GQkZGziCX3XjSVtYkb4jBfph4jIyMfKosaZdZBKLbnLJMBJKyVS0pQUZGTiR364AbXbRhxqClbPruKChkZOQ08pq0tTY6X5HseLi2G0WIjIx8vBx8/Xjz09+qdehvRiUjIyPnkduy39GGuOoHsoQK9lCThoyMnEZ+23TfbBJyOK5aH2berUjIyMgHyhpidWTT+aJDgdaQLDIychq5h7E/xWOwocCsLgWr5ekESWRk5ANl3fVIE1xM4tQeMrw9HL7QOzIychbZT3zToxNiC612u/jX6chTZGTk8+XubGjYD0exNHu20QwvMjJyInntwV8a4uLwUm2y8wFByMjISWSNinQ9R9IzvDHMonul/v5MW2Rk5PNkPwVSx6RbnlfLRbyy/SniioyMfKrszyr2xFJ7qFe3m8MfhoyMnET2ExZC0WnYB9l65SsSMjJyInnpf4lT0H3sz9z7fOzQR0ZGPlX2xrdNE9xa3N58EFB77NBHRkY+UbY3rWhu96FDX2vXn09YQEZGPlBucQJh7Nb3ice6IYrhVmRk5JTympfRdrnuxzHEIerIyMgp5dh1q4HW3ZFwlqZBRkZOIneLh9hx9j47fbP+tBKKVJGRkTPIcdxxteuaoLFOmeVmZGTkLPJffyEjIyMjIyMjIyMjIyMjIyMjIyMjp5N/A7C8eKsv3GtDAAAAAElFTkSuQmCC",
-      },
-      {
-        qrName: "Sucursal San Justo - QR SJ 2",
-        qr: "iVBORw0KGgoAAAANSUhEUgAAAsUAAALFAQAAAAADsKO1AAAEF0lEQVR42u3dy44bIRCFYd6A939LltmRkdoN5xS47UiJFFG/FUWenvbnWaGmbpT+j16/CjIyMjIyMjIyMjIyMjIyMjIyMjLyfy23Ml7Xj68rtV///7xev7pu/rleX2/mzePjyMjIOeT55rrz0oYsn53s0NriICMjZ5Cb3KP4ffH68bpyrTx2Q/OvQEZGTiW3uS3SB5Xxq+0jDTIycmp5LDt1fjbsj2wH1JGRkbPKugO6r1w3v9acZQmaH/x+b4WMjHyQbGGQP//3VSwXGRn5JNlfIztjS5DEY0ceRyMq3+SOkZGRz5HbXI5e0dSx2Qkv3/jYLqnKtyAjIx8vy/5l3jbWpTvVO2KwuhzNiCsyMnIyeWixCOReoAy/FytN/u4jrsjIyOfKWnoaikPmZ6utSPEvqR0ZGTmRrJsaKT21avZQSKZpX42rICMjp5F1FzMSLjEeIjlf2yK1JfqKjIycQ57hU4mZzHCKt72sKZunzhdkZOQTZVt2JEgytW6p3nlDtcpVZGTkRLJXp9uGqNhzjjXTaQPdmwp2ZGTkM+Xu6drewyOK7YO2Gd7+VBmCjIx8pKydcfqgMnO4d0o3boI0wPKYO0ZGRj5MXks+1qIyS9zIxudD7hgZGflIWRrztfNFm2pD160OGYv9tsjIyGnkoMWull0F+7xSPWyLjIx8vNytpjTMBdLiMQ3GhnqzfYYXGRn5YNlr1+0JR/M4a0lq2Y0GQkZGziCX3XjSVtYkb4jBfph4jIyMfKosaZdZBKLbnLJMBJKyVS0pQUZGTiR364AbXbRhxqClbPruKChkZOQ08pq0tTY6X5HseLi2G0WIjIx8vBx8/Xjz09+qdehvRiUjIyPnkduy39GGuOoHsoQK9lCThoyMnEZ+23TfbBJyOK5aH2berUjIyMgHyhpidWTT+aJDgdaQLDIychq5h7E/xWOwocCsLgWr5ekESWRk5ANl3fVIE1xM4tQeMrw9HL7QOzIychbZT3zToxNiC612u/jX6chTZGTk8+XubGjYD0exNHu20QwvMjJyInntwV8a4uLwUm2y8wFByMjISWSNinQ9R9IzvDHMonul/v5MW2Rk5PNkPwVSx6RbnlfLRbyy/SniioyMfKrszyr2xFJ7qFe3m8MfhoyMnET2ExZC0WnYB9l65SsSMjJyInnpf4lT0H3sz9z7fOzQR0ZGPlX2xrdNE9xa3N58EFB77NBHRkY+UbY3rWhu96FDX2vXn09YQEZGPlBucQJh7Nb3ice6IYrhVmRk5JTympfRdrnuxzHEIerIyMgp5dh1q4HW3ZFwlqZBRkZOIneLh9hx9j47fbP+tBKKVJGRkTPIcdxxteuaoLFOmeVmZGTkLPJffyEjIyMjIyMjIyMjIyMjIyMjIyMjp5N/A7C8eKsv3GtDAAAAAElFTkSuQmCC",
-      },
-    ];
     downloadMultipleQR(dataDownloadQR);
+  };
+
+  const onGenerateElementSimpleQR = () => {
+    // generateElementSimpleQRCode(
+    //   dataDownloadQR[0].qr,
+    //   `${dataDownloadQR[0].qrName}`,
+    //   formState as formData
+    // ).then((b64) => {
+    //   console.log("Imagen", b64);
+    //   setbase64(b64);
+    //   setVisible(true);
+    // });
+    setVisible(true);
+  };
+
+  const onGenerateElementDobleQR = () => {
+    generateElementDobleQRCode(dataDownloadQR).then((b64) => {
+      console.log("Imagen", b64);
+      setbase64(b64);
+      setVisible(true);
+    });
+  };
+
+  const onGenerate = () => {
+    console.log("form formState", formState);
+    generateElementSimpleQRCode(
+      dataDownloadQR[0].qr,
+      `${dataDownloadQR[0].qrName}`,
+      formState as formData
+    ).then((b64: any) => {
+      console.log("Imagen", b64);
+      setbase64(b64);
+      // setVisible(true);
+    });
   };
 
   return (
@@ -51,12 +108,83 @@ const DownloadQrPage = () => {
             Doble QR download
           </Button>
         </Grid>
+        <Grid>
+          <Button color="gradient" onClick={onGenerateElementSimpleQR}>
+            Generate Element Simple QR
+          </Button>
+        </Grid>
+        <Grid>
+          <Button color="gradient" onClick={onGenerateElementDobleQR}>
+            Generate Element Doble QR
+          </Button>
+        </Grid>
       </Grid.Container>
-      <Card css={{ mw: "400px" }}>
+      {/* <Card css={{ mw: "400px" }}>
         <Card.Body>
           <Text>A basic card</Text>
+          <img src={base64} alt="" />
         </Card.Body>
-      </Card>
+      </Card> */}
+      <Modal
+        scroll
+        width="900px"
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+        {...bindings}
+      >
+        <Modal.Header>
+          <Text id="modal-title" size={18}>
+            <Container>
+              ContentQR:
+              <input
+                type="text"
+                placeholder="xContentQR"
+                name="xContentQR"
+                value={xContentQR}
+                onChange={onInputChange}
+              />
+              <input
+                type="text"
+                placeholder="yContentQR"
+                name="yContentQR"
+                value={yContentQR}
+                onChange={onInputChange}
+              />
+            </Container>
+            <Container>
+              NameQR:
+              <input
+                type="text"
+                placeholder="xNameQR"
+                name="xNameQR"
+                value={xNameQR}
+                onChange={onInputChange}
+              />
+              <input
+                type="text"
+                placeholder="yNameQR"
+                name="yNameQR"
+                value={yNameQR}
+                onChange={onInputChange}
+              />
+            </Container>
+            <button onClick={onGenerate}>Generate</button>
+          </Text>
+        </Modal.Header>
+        <Modal.Body>
+          <div id="modal-description">
+            <img src={base64} alt="" />
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button auto flat color="error" onPress={() => setVisible(false)}>
+            Close
+          </Button>
+          <Button auto onPress={() => setVisible(false)}>
+            Agree
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Layout>
   );
 };
--- react-random/galicia-app-ts/utils/download-qr-code.ts
+++ react-random/galicia-app-ts/utils/download-qr-code.ts
@@ -1,4 +1,5 @@
 import mergeImages from "merge-images";
+import { formData } from "../pages/download-qr";
 import { QR_NAVE_B64 } from "./qr-code-template";
 import { QR_NAVE_DOUBLE } from "./qr-code-template-doble";
 
@@ -53,6 +54,56 @@ export function downloadMultipleQR(content: any[]) {
   });
 }
 
+export function generateElementDobleQRCode(content: any[]) {
+  const firstQR = content[0];
+  const secondQR = content[1];
+  console.log(firstQR);
+  console.log(secondQR);
+  return new Promise((resolve, reject) => {
+    const imgTextA = getTextBase64({
+      c: document.createElement("canvas"),
+      qrName: firstQR.qrName,
+    });
+    const imgTextB = getTextBase64({
+      c: document.createElement("canvas"),
+      qrName: secondQR.qrName,
+    });
+
+    mergeImages([
+      { src: QR_NAVE_DOUBLE },
+      {
+        src: `data:image/png;base64,${firstQR.qr}`,
+        x: 280,
+        y: 450,
+      },
+      {
+        src: `data:image/png;base64,${secondQR.qr}`,
+        x: 1520,
+        y: 450,
+      },
+      {
+        src: `${imgTextA}`,
+        x: 160,
+        y: 1120,
+      },
+      {
+        src: `${imgTextB}`,
+        x: 1405,
+        y: 1120,
+      },
+    ])
+      .then((b64) => {
+        let a = document.createElement("a");
+        a.href = b64;
+
+        resolve(a);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
 function getTextBase64({ c, width = 850, height = 100, qrName }: any) {
   const ctx = c.getContext("2d");
   c.width = width;
@@ -93,6 +144,42 @@ export function downloadQRCode(content: string, qrName: string) {
         a.download = `${qrName}.png`;
         a.click();
         resolve(true);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+export function generateElementSimpleQRCode(
+  content: string,
+  qrName: string,
+  data: formData
+): Promise<unknown> {
+  return new Promise((resolve, reject) => {
+    const c = document.createElement("canvas");
+    const base64Canvas = getTextBase64({ c, qrName });
+
+    mergeImages([
+      {
+        src: QR_NAVE_B64,
+      },
+      {
+        src: `data:image/png;base64,${content}`,
+        x: data.xContentQR,
+        y: data.yContentQR,
+      },
+      {
+        src: `${base64Canvas}`,
+        x: data.xNameQR,
+        y: data.yNameQR,
+      },
+    ])
+      .then((b64) => {
+        let a = document.createElement("a");
+        a.href = b64;
+
+        resolve(a);
       })
       .catch((error) => {
         reject(error);
