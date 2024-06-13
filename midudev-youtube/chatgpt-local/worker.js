d9a69d086a0484b3d24a8b7fdb4a1b848625f8ae
Fernando Miras <fmiras@simtlix.com>
2024-06-13 12:39:18 -0300

    curso

--- midudev-youtube/chatgpt-local/worker.js
+++ midudev-youtube/chatgpt-local/worker.js
@@ -3,6 +3,8 @@ import { MLCEngineWorkerHandler, MLCEngine } from "https://esm.run/@mlc-ai/web-l
 const engine = new MLCEngine()
 const handler = new MLCEngineWorkerHandler(engine)
 
+// Cuando resive un evento
 onmessage = msg => {
+  console.log('Worker: Message received from main thread');
   handler.onmessage(msg)
 }
