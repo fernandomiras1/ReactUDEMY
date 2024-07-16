98376ad3ea300f1d70e09d85d98bb9f4ead896cc
Fernando Miras <f.miras@electroneek.com>
2024-07-16 20:44:37 -0300

    nextjs 2024 13+

--- React-Fernando-Herrera-2022/next-2024-(v13+)/next-my-dashboard/next.config.js
+++ React-Fernando-Herrera-2022/next-2024-(v13+)/next-my-dashboard/next.config.js
@@ -1,19 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    // Aca acepatmos las URL de la imagen, para que se llame del lado del servidor
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com'
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com'
-      }
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+    ],
+  },
+};
 
-    ]
-  }
-
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
