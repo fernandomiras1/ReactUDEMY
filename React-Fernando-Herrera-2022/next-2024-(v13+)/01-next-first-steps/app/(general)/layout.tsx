78dcf627d3cd98d44f9f9de1771404ced9792b28
Fernando Miras <f.miras@electroneek.com>
2024-07-16 19:57:53 -0300

    nextjs 2024 13+

--- React-Fernando-Herrera-2022/next-2024-(v13+)/01-next-first-steps/app/(general)/layout.tsx
+++ React-Fernando-Herrera-2022/next-2024-(v13+)/01-next-first-steps/app/(general)/layout.tsx
@@ -1,18 +1,25 @@
 import { Navbar } from "@/components";
 
-
+/**
+ *
+ * @param (general)
+ *
+ * SI pones (general entre parentensis), le vas a indicar a next, que no queres que esa parte de la URL, y que sea un agrupador.
+ *
+ * @returns
+ */
 
 export default function GeneralLayout({
- children
+  children,
 }: {
- children: React.ReactNode;
+  children: React.ReactNode;
 }) {
   return (
     <>
       <Navbar />
       <main className="flex flex-col items-center p-24">
         <span className="text-lg">Hola Mundo</span>
-        { children }
+        {children}
       </main>
     </>
   );
